532eade3c60653597d0843cb50f238b2b12f8ba7
NVerner <99597964+Varnhover@users.noreply.github.com>
2023-09-28 22:35:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -105,6 +105,8 @@ print(rdkit.Chem.Draw.MolsToImage(mols))
 s = rdkit.Chem.Draw.MolsToImage(mols)
 st.image(s)
 im = rdkit.Chem.Draw.MolsToGridImage(mols[:8], molsPerRow=4, subImgSize=(300,200))
+im.format = 'svg'
+im.mode = 'RGBA'
 st.image(im)
 
 os.system('python eToxPred/etoxpred_predict.py --datafile test.smi --modelfile dbs/etoxpred_best_model.joblib --outputfile results.csv')
