8c0b0fa2e2f7d3f33ae5124f565e49441e3d44c7
NVerner <99597964+Varnhover@users.noreply.github.com>
2023-10-02 11:00:54 +0300

    Create eToxPred.py

---
+++ pages/eToxPred.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import os
+
+st.set_page_config(page_title="D4Tool",page_icon="💊")
+
+
+st.button("eToxPred")
+
+#f = open('results.csv', 'w')
+#f.write("1")
+
+
+if st.button:
+    os.system(
+        'streamlit run ToxPred/etoxpred_predict.py [--datafile test.smi] [--modelfile dbs/etoxpred_best_model.joblib] [--outputfile results.csv] --browser.gatherUsageStats false')
+    st.write("Kapec", pd.read_csv('results.csv'))
