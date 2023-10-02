cef4bbf44e9cceadfec74aae56ef3369882f7392
NVerner <99597964+Varnhover@users.noreply.github.com>
2023-10-02 11:05:46 +0300

    Update eToxPred.py

--- pages/eToxPred.py
+++ pages/eToxPred.py
@@ -5,13 +5,13 @@ import os
 st.set_page_config(page_title="D4Tool",page_icon="💊")
 
 
-st.button("eToxPred")
+t = st.button("eToxPred")
 
 #f = open('results.csv', 'w')
 #f.write("1")
 
 
-if st.button:
+if t:
     st.write("ec")
     os.system('streamlit run ToxPred/etoxpred_predict.py [--datafile test.smi] [--modelfile dbs/etoxpred_best_model.joblib] [--outputfile results.csv] --browser.gatherUsageStats false')
     st.write("Kapec", pd.read_csv('results.csv'))
